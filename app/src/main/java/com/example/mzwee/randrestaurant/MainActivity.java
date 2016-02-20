9e63d27eff84fe0d77169559034010b8a35ce3c8
mzwee <mzwee95@gmail.com>
2016-02-20 12:30:12 -0500

    first commit with user login/registration working

---
+++ app/src/main/java/com/example/mzwee/randrestaurant/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.mzwee.randrestaurant;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Firebase ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Firebase set up
+        Firebase.setAndroidContext(this);
+        ref = new Firebase("https://hackillinois16.firebaseio.com/");
+
+        ref.child("message").setValue("Do you have data? You'll love Firebase.");
+
+        ref.child("message").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/mzwee/randrestaurant/UserAuth.java
@@ -0,0 +1,105 @@
+package com.example.mzwee.randrestaurant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
+
+/**
+ * Created by mzwee on 2/20/16.
+ */
+public class UserAuth extends AppCompatActivity{
+    Firebase ref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_auth);
+        final EditText email = (EditText) findViewById(R.id.email);
+        final EditText password = (EditText) findViewById(R.id.password);
+        final Button login = (Button) findViewById(R.id.login);
+        final TextView forgot_password = (TextView) findViewById(R.id.forgot_password);
+        final Button register = (Button) findViewById(R.id.register);
+
+        // Set up Firebase
+        Firebase.setAndroidContext(this);
+        ref = new Firebase("https://hackillinois16.firebaseio.com/");
+
+        // Login function
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.authWithPassword(email.getText().toString(), password.getText().toString(), new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) { // login
+                        // if our login is true we should go to the main page
+                        Toast.makeText(getApplicationContext(), "Log in approved!",
+                                Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(UserAuth.this, MainActivity.class);
+                        startActivity(intent);
+
+                        System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        Toast.makeText(getApplicationContext(), "Log in failed:( " + firebaseError.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                        Log.e("User", "Login failed: " + firebaseError.getMessage());
+                    }
+                });
+            }
+        });
+
+        //Registration function
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.createUser(email.getText().toString(), password.getText().toString(), new Firebase.ValueResultHandler<Map<String, Object>>() {
+                    @Override
+                    public void onSuccess(Map<String, Object> stringObjectMap) {
+                        Toast.makeText(getApplicationContext(), "You have been registered!",
+                                Toast.LENGTH_LONG).show();
+                        System.out.println("Successfully created user account with uid: " + stringObjectMap.get("uid"));
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        Toast.makeText(getApplicationContext(), "There was an error in your registration. " + firebaseError.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                        Log.e("User", "Registration failed: " + firebaseError.getMessage());
+                    }
+                });
+            }
+        });
+
+        //Forgot password function
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.resetPassword(email.getText().toString(), new Firebase.ResultHandler() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
+
