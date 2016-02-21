d316981c081a96d179bc52e0b0544ee72655ecc6
mzwee <mzwee95@gmail.com>
2016-02-21 07:38:46 -0500

    added Business detail pull

--- app/src/main/java/com/example/mzwee/randrestaurant/YelpQuery.java
+++ app/src/main/java/com/example/mzwee/randrestaurant/YelpQuery.java
@@ -12,11 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 /**/
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.common.ConnectionResult;
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.location.LocationServices;
 import com.yelp.clientlib.connection.YelpAPI;
 import com.yelp.clientlib.connection.YelpAPIFactory;
 import com.yelp.clientlib.entities.Business;
@@ -28,23 +28,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import retrofit.Call;
 
 /**
  * Created by mzwee on 2/20/16.
  */
-public class YelpQuery extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class YelpQuery extends AppCompatActivity {
     //    implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
     String consumerKey = "1jsFUVTrTBf7fWReBxt8Xg";
     String consumerSecret = "wNBLG0Sz05SOdWtWYOR5xGcTC1U";
     String token = "H4MaKPlMJvWZgsahXPPBKZcFza_-InqA";
     String tokenSecret = "TrXf9SUarkRcQqIrWKG0LCwn6q4";
     ListView listView;
-    int PERMISSION_LOCATION_REQUEST_CODE = 1;
 
     // Provides the entry point to Google Play Services
-    protected GoogleApiClient mGoogleApiClient;
+//    protected GoogleApiClient mGoogleApiClient;
 
     // Represents a geographical location
     protected Location mLastLocation;
@@ -58,18 +58,7 @@ public class YelpQuery extends AppCompatActivity implements GoogleApiClient.Conn
         super.onCreate(savedInstanceState);
         setContentView(R.layout.yelp_query);
 
-        buildGoogleApiClient();
-
-        // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
-            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
-            // See https://g.co/AppIndexing/AndroidStudio for more information.
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .addApi(AppIndex.API).build();
-        }
+//        buildGoogleApiClient();
 
         // Activate StrictMode
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -86,20 +75,30 @@ public class YelpQuery extends AppCompatActivity implements GoogleApiClient.Conn
 
         final List<String> businessNames = new ArrayList<String>();
 
-        CoordinateOptions coordinate;
-        if(mLastLocation != null) {
-            coordinate = CoordinateOptions.builder().latitude(mLastLocation.getLatitude()).longitude(mLastLocation.getLongitude()).build();
-        } else {
-            coordinate = CoordinateOptions.builder().latitude(40.109387).longitude(-88.2272456).build();
-        }
+        CoordinateOptions coordinate = CoordinateOptions.builder().latitude(40.1).longitude(-88.23).build();
+//        if(mLastLocation != null) {
+//            coordinate = CoordinateOptions.builder().latitude(mLastLocation.getLatitude()).longitude(mLastLocation.getLongitude()).build();
+//        } else {
+//            coordinate = CoordinateOptions.builder().latitude(40.109387).longitude(-88.2272456).build();
+//        }
         Call<SearchResponse> call = yelpAPI.search(coordinate, params);
         try {
             SearchResponse searchResponse = call.execute().body();
-            ArrayList<Business> businesses = searchResponse.businesses();
-            for (Business b : businesses) {
-                businessNames.add(b.name());
-            }
-            System.out.println(businessNames.size());
+            Random r = new Random();
+            int value = r.nextInt(20);
+            Business selected = searchResponse.businesses().get(value);
+            businessNames.add(selected.name());
+            businessNames.add(selected.phone());
+            businessNames.add(Double.toString(selected.reviewCount()));
+            businessNames.add(Double.toString(selected.rating()));
+            businessNames.add(selected.location().displayAddress().toString());
+            businessNames.add(selected.imageUrl());
+
+//            ArrayList<Business> businesses = searchResponse.businesses();
+//            for (Business b : businesses) {
+//                businessNames.add(b.name());
+//            }
+//            System.out.println(businessNames.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -111,89 +110,89 @@ public class YelpQuery extends AppCompatActivity implements GoogleApiClient.Conn
     }
 
     // Builds a GoogleApiClient. Uses the addApi() to request the LocationServices API
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
+//    protected synchronized void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+//    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "YelpQuery Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.mzwee.randrestaurant/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "YelpQuery Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.mzwee.randrestaurant/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    // Runs when a GoogleApiClient object successfully connects
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            askForPermission();
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLastLocation == null) {
-            System.out.println("No location detected");
-        }
-    }
-
-    private void askForPermission() {
-        String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSION_LOCATION_REQUEST_CODE);
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        mGoogleApiClient.connect();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mGoogleApiClient.connect();
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "YelpQuery Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app deep link URI is correct.
+//                Uri.parse("android-app://com.example.mzwee.randrestaurant/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "YelpQuery Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app deep link URI is correct.
+//                Uri.parse("android-app://com.example.mzwee.randrestaurant/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
+//        if (mGoogleApiClient.isConnected()) {
+//            mGoogleApiClient.disconnect();
+//        }
+//    }
+//
+//    // Runs when a GoogleApiClient object successfully connects
+//    @Override
+//    public void onConnected(Bundle connectionHint) {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+////            askForPermission();
+//            return;
+//        }
+//        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//        if(mLastLocation == null) {
+//            System.out.println("No location detected");
+//        }
+//    }
+//
+//    private void askForPermission() {
+//        String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+//        ActivityCompat.requestPermissions(
+//                this,
+//                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+//                PERMISSION_LOCATION_REQUEST_CODE);
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(ConnectionResult result) {
+//
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int cause) {
+//        mGoogleApiClient.connect();
+//    }
 }
